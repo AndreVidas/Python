596dc7d9b3d851a572a3c2d824d3ce2b46f44712
AndreVidas <andre.v.olsen@gmail.com>
2018-06-01 23:29:58 +0200

    make one round programming.

--- ML/Kmeans.py
+++ ML/Kmeans.py
@@ -8,11 +8,14 @@ Created on Fri Jun  1 14:42:22 2018
 import numpy as np
 
 
+
 def Kmeans(data, K):
+    
     
     
     # pick K random centroids 
     K_coord = np.random.rand(K,2)*10
+    
     
     # label each data point to the closest centroid.
     label = []
@@ -23,32 +26,34 @@ def Kmeans(data, K):
             dist.append(np.sqrt( (data[i,0] - K_coord[j,0])**2 + (data[i,1] - K_coord[j,1])**2 ))
         label.append(np.argmin(dist))
         
-        #print(dist)
-        #print(label)
         
     
     # calculate new coordinates for each centroid by finding the average
     # coordinate for every data points labelled to the specific centroid 
     # 
-    #print(K_coord.shape[0])
-    for i in range(K_coord.shape[0]):
-        sumCoords = np.array # TO BE CONTINUED... MAYBE: FIND OUT HOW TO MAKE ARRAY TO BE EXPANDED...
-        for j in range(data.shape[0]):
-            countMembers = label.count(j)
-            if label[j] = i:
-                
-                
-                
-        
+    new_K_coord = np.zeros([K,2]) # allocate data for new K centriod coordinates
+    sumCoords = np.zeros([data.shape[0],2])
+    for i in range(0,K):
+        for j in range(0,data.shape[0]):
+            #countMembers = label.count(j)
+            if label[j] == i:
+                sumCoords[j,:] = data[j,:]
+            
     
-    return 0
+        new_K_coord[i,] = np.sum(sumCoords, axis = 0)
+    
+    
+          
+    
+    return np.hstack((data,np.asarray(label).reshape(data.shape[0],1)))
 
 
 
 ### main ####
-data = np.random.rand(50,2)*10
+np.random.seed(10)
+data = np.random.rand(20,2)*10
 #print(data)
 
 
 #print(Kmeans(data, 5))
-Kmeans(data, 2)
+Kmeans(data, 3)
